6f33f4dfc20cedcd1871e5132235e96f79c108e2
dscription <dscriptionplayground@gmail.com>
2021-01-26 14:26:16 -0500

    create global variables and remove const

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
+  let container;
+  let customer;
+
   it('renders the customer first name', () => {
-    const customer = { firstName: 'Ashley' };
-    const container = document.createElement('div');
+    customer = { firstName: 'Ashley' };
+    container = document.createElement('div');
     ReactDOM.render(<Appointment customer={customer} />, container);
     expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
-    const customer = { firstName: 'Jordan' };
-    const container = document.createElement('div');
+    customer = { firstName: 'Jordan' };
+    container = document.createElement('div');
     ReactDOM.render(<Appointment customer={customer} />, container);
     expect(container.textContent).toMatch('Jordan');
   });
 });
-
