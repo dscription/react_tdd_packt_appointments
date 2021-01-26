a71f63711abc0a48d48261c078046f377e953153
dscription <dscriptionplayground@gmail.com>
2021-01-26 14:36:15 -0500

    refactor render function

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -6,19 +6,21 @@ describe('Appointment', () => {
   let container;
   let customer;
 
+  const render = (component) => ReactDOM.render(component, container);
+
   beforeEach(() => {
-    container = document.createElement('div')
-  })
+    container = document.createElement('div');
+  });
 
   it('renders the customer first name', () => {
     customer = { firstName: 'Ashley' };
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
     customer = { firstName: 'Jordan' };
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jordan');
   });
 });
