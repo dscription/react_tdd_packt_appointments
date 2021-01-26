f6b747bba09c653933196400bea60174c348ea28
dscription <dscriptionplayground@gmail.com>
2021-01-26 14:14:20 -0500

    render another customer first name

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Appointment} from '../src/Appointment'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
   it('renders the customer first name', () => {
-    const customer = {firstName: 'Ashley'}
-    const component = <Appointment customer={customer} />
-    const container = document.createElement('div')
-    document.body.appendChild(container)
+    const customer = { firstName: 'Ashley' };
+    const container = document.createElement('div');
+    ReactDOM.render(<Appointment customer={customer} />, container);
+    expect(container.textContent).toMatch('Ashley');
+  });
 
-    ReactDOM.render(component, container)
+  it('renders another customer first name', () => {
+    const customer = { firstName: 'Jordan' };
+    const container = document.createElement('div');
+    ReactDOM.render(<Appointment customer={customer} />, container);
+    expect(container.textContent).toMatch('Jordan');
+  });
+});
 
-    expect(document.body.textContent).toMatch('Ashley')
-  })
-})
