bbf8f3ada85980b0df2d7cd7430f916705160a22
dscription <dscriptionplayground@gmail.com>
2021-01-26 14:32:41 -0500

    use beforeEach to create container

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -6,16 +6,18 @@ describe('Appointment', () => {
   let container;
   let customer;
 
+  beforeEach(() => {
+    container = document.createElement('div')
+  })
+
   it('renders the customer first name', () => {
     customer = { firstName: 'Ashley' };
-    container = document.createElement('div');
     ReactDOM.render(<Appointment customer={customer} />, container);
     expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
     customer = { firstName: 'Jordan' };
-    container = document.createElement('div');
     ReactDOM.render(<Appointment customer={customer} />, container);
     expect(container.textContent).toMatch('Jordan');
   });
