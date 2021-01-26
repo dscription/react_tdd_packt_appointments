7e7f1fcdb10f8ba56b1e3f4bd905e009ba3799f3
dscription <dscriptionplayground@gmail.com>
2021-01-26 16:39:35 -0500

    renders each appointment in an li

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
@@ -49,4 +49,18 @@ describe('AppointmentsDayView', () => {
     expect(container.querySelector('ol')).not.toBeNull();
     expect(container.querySelector('ol').children).toHaveLength(2);
   });
+
+  it('renders each appointment in an li', () => {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) },
+    ];
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+    expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+  });
 });
+
+
